6676e3a78c2155db7ae8bf8fa00d0df4e8254958
onehana1 <73771420+onehana1@users.noreply.github.com>
2022-09-19 11:52:32 +0900

    Update draw_grid.py

--- draw_grid.py
+++ draw_grid.py
@@ -3,13 +3,20 @@ import turtle
 turtle.reset()
 
 # draw horizontal lines - 수평선
-y = 0
-while y <= 500:
+y = -100
+while y <= 400:
     turtle.penup()
-    turtle.goto(0,y)
+    turtle.goto(-100,y)
     turtle.pendown()
-    turtle.goto(500,y)
+    turtle.goto(400,y)
     y += 100
     
 
 # draw vertical lines - 수직선
+x = -100
+while x <= 400:
+    turtle.penup()
+    turtle.goto(x,-100)
+    turtle.pendown()
+    turtle.goto(x,400)
+    x += 100
