217472451fe7892b08a175ca4307325b2f541c12
kimjinyoung <wlsdudkim9693@gmail.com>
2022-09-28 11:03:19 +0900

    Update character_runs.py

--- 06_anime/character_runs.py
+++ 06_anime/character_runs.py
@@ -3,16 +3,15 @@ open_canvas()
 grass = load_image('grass.png')
 character = load_image('run_animation.png')
 
-x = 0
+
 frame = 0
 
-while (x < 800):
+for x in range(0, 800, 5):
     clear_canvas()
     grass.draw(400, 30)
     character.clip_draw(frame * 100, 0, 100, 100, x, 90)
     update_canvas()
     frame = (frame + 1) % 8
-    x += 5
     delay(0.05)
     get_events()
 
